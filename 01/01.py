01b4830517d5131462ac684c280fd4bc6b900b9b
Kent D <lolexplode@gmail.com>
2021-12-01 11:43:48 +0100

    use *args

--- 01/01.py
+++ 01/01.py
@@ -1,3 +1,4 @@
-f=lambda l:print(sum(a<b for a,b in zip(l,l[1:])))
-f(p:=list(map(int,open("in"))))
-f(list(map(sum,zip(p,p[1:],p[2:]))))
+f=lambda *l:print(sum(a<b for a,b in zip(l,l[1:])))
+p=list(map(int,open("in")))
+f(*p)
+f(*map(sum,zip(p,p[1:],p[2:])))
