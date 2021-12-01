2529e3ff6ef321bd8ef6b0f75423d2fa93d6393e
Kent D <lolexplode@gmail.com>
2021-12-01 11:39:41 +0100

    walrus operator for p, and pass part 2 directly into f

--- 01/01.py
+++ 01/01.py
@@ -1,5 +1,3 @@
 f=lambda l:print(sum(a<b for a,b in zip(l,l[1:])))
-p=list(map(int,open("in")))
-s=list(map(sum,zip(p,p[1:],p[2:])))
-f(p)
-f(s)
+f(p:=list(map(int,open("in"))))
+f(list(map(sum,zip(p,p[1:],p[2:]))))
