0179ee82662e2df8e87309e8ba76a075e6c19dd5
Kent D <kent.daleng@nav.no>
2021-12-02 14:03:33 +0100

    golfing a bit here too

--- 02/prog.py
+++ 02/prog.py
@@ -1,5 +1,5 @@
 a=h=d=0
-for k,v in((l.split()[0][0],int(l.split()[1]))for l in open(0)):
- if k=="f":h+=v;d+=v*a 
+for k,v in((k[0],int(v))for k,v in map(str.split,open(0))):
+ if k=="f":h+=v;d+=v*a
  else:a+=v if k=="d"else-v
 print(h*a,d*h)
