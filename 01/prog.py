712247edcec0cf9bc7c9e409902551b5f4e19be8
Kent D <kent.daleng@nav.no>
2021-12-02 10:14:54 +0100

    print without newlines between part1 and 2

--- 01/prog.py
+++ 01/prog.py
@@ -1,4 +1,3 @@
-f=lambda*l:print(sum(a<b for a,b in zip(l,l[1:])))
+f=lambda*l:sum(a<b for a,b in zip(l,l[1:]))
 p=list(map(int,open(0)))
-f(*p)
-f(*map(sum,zip(p,p[1:],p[2:])))
+print(f(*p),f(*map(sum,zip(p,p[1:],p[2:]))))
--- 02/prog.py
+++ 02/prog.py
@@ -2,4 +2,4 @@ a=h=d=0
 for k,v in((l.split()[0][0],int(l.split()[1]))for l in open(0)):
  if k=="f":h+=v;d+=v*a 
  else:a+=v if k=="d"else-v
-print(f"{h*a}\n{d*h}")
+print(h*a,d*h)
