37bad8f1253143560e3e38ba747623d4d3aaf78b
Kent D <kent.daleng@nav.no>
2021-12-05 13:06:46 +0100

    remove values call

--- 05/prog.py
+++ 05/prog.py
@@ -1,5 +1,5 @@
 import re,collections as s;a=s.defaultdict(int);b=a.copy()
 for x,y,v,w in(map(int,re.findall('\d+',x))for x in open(0)):
  for p in range(-~max(abs(v-x),abs(w-y))):c,d=(v-x>0)-(v-x<0),(w-y>0)-(w-y<0);h=x+c*p,y+d*p;b[h]+=1;a[h]+=1^c&d
-f=lambda x:sum(v>1 for v in x.values())
+f=lambda x:sum(x[v]>1 for v in x)
 print(f(a),f(b))
